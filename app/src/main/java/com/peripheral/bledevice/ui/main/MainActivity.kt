6498863295d88bbc6955a6ec1a29e1a34e110c37
nazmul <nhasan@ael-bd.com>
2024-05-06 17:57:38 +0600

    Removed unused code

--- app/src/main/java/com/peripheral/bledevice/ui/main/MainActivity.kt
+++ app/src/main/java/com/peripheral/bledevice/ui/main/MainActivity.kt
@@ -3,11 +3,7 @@ package com.peripheral.bledevice.ui.main
 import android.Manifest
 import android.annotation.SuppressLint
 import android.bluetooth.le.ScanResult
-import android.content.ComponentName
-import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Bundle
-import android.os.IBinder
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -22,15 +18,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.lightnotebook.data.database.entity.DeviceEntity
-import com.mnh.service.BluetoothScanService
 import com.mnh.service.model.LockRSSI
 import com.napco.utils.PermissionManager.Companion.permissionManager
 import com.peripheral.bledevice.ui.navigation.Navigation
@@ -40,7 +33,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    private var bluetoothScanService: BluetoothScanService? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,31 +75,6 @@ class MainActivity : ComponentActivity() {
         Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
     }
 
-    private fun startBluetoothScanForegroundService() {
-        val bleScanServiceConnectionCallback = object : ServiceConnection {
-            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                val binder = service as? BluetoothScanService.MyBinder
-                bluetoothScanService = binder?.getService()
-
-                setContent {
-                    MainContentWithService(service = bluetoothScanService!!)
-                }
-
-            }
-
-            override fun onServiceDisconnected(name: ComponentName?) {
-                showToast("Service disconnected")
-            }
-        }
-
-        fun startBleScanningService(bleScanServiceConnectionCallback: ServiceConnection) {
-            val serviceIntent = Intent(this, BluetoothScanService::class.java)
-            startForegroundService(serviceIntent)
-            bindService(serviceIntent, bleScanServiceConnectionCallback, BIND_AUTO_CREATE)
-        }
-
-        startBleScanningService(bleScanServiceConnectionCallback)
-    }
 
 }
 
@@ -190,21 +157,6 @@ fun ListItem(
         Spacer(modifier = Modifier.height(20.dp))
     }
 }
-
-@Composable
-fun MainContentWithService(
-    service: BluetoothScanService, viewModel: MainActivityViewModel = viewModel()
-) {
-    val locks by viewModel.locks.collectAsState(initial = emptyList())
-    val lockInfo by service.lockRSSI.collectAsState(initial = LockRSSI(""))
-
-    service.deviceName = viewModel.deviceName
-
-    MainContentBodyWithService(list = locks, lockInfo, viewModel) {
-        viewModel.insert(viewModel.deviceName)
-    }
-}
-
 
 @Composable
 fun MainContentBodyWithService(
