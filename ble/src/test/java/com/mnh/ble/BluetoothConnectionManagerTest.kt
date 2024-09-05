a760b0f52ff03fe4a79d4084eb851596e72954ec
nazmul <mnhmasum@gmail.com>
2024-09-05 21:53:40 +0600

    Test Bluetooth Connection Result

--- ble/src/test/java/com/mnh/ble/BluetoothConnectionManagerTest.kt
+++ ble/src/test/java/com/mnh/ble/BluetoothConnectionManagerTest.kt
@@ -3,9 +3,20 @@ package com.mnh.ble
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattService
 import android.bluetooth.BluetoothProfile
 import android.content.Context
 import com.mnh.ble.bluetooth.bleconnection.BleConnectionManagerImpl
+import com.napco.utils.Utility
+import com.napco.utils.model.Characteristic
+import com.napco.utils.model.Service
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.Before
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.anyString
@@ -22,9 +33,16 @@ class BluetoothConnectionManagerTest {
 
     private lateinit var bleConnectionManager: BleConnectionManagerImpl
 
+    private val dispatcher = TestCoroutineDispatcher()
+    //private val mockScope = TestCoroutineScope(dispatcher)
+
+    private val mockScope = CoroutineScope(Dispatchers.Unconfined)
+
+
     @Before
     fun setUp() {
-        bleConnectionManager = BleConnectionManagerImpl(mockContext, mockBluetoothAdapter)
+        bleConnectionManager =
+            BleConnectionManagerImpl(mockContext, mockScope, mockBluetoothAdapter)
     }
 
     @Test
@@ -49,5 +67,109 @@ class BluetoothConnectionManagerTest {
 
         Mockito.verify(mockBluetoothGatt).discoverServices()
     }
+
+    /* @Test
+     fun `test onServicesDiscovered success`(): Unit = runBlockingTest{
+         val status = BluetoothGatt.GATT_SUCCESS
+
+         val mockBluetoothGatt = mock(BluetoothGatt::class.java)
+
+         val mockDevice = mock(BluetoothDevice::class.java)
+
+         Mockito.`when`(mockBluetoothGatt.device).thenReturn(mockDevice)
+
+         Mockito.`when`(mockBluetoothGatt.device.name).thenReturn("abc")
+         Mockito.`when`(mockBluetoothGatt.device.address).thenReturn("address")
+         Mockito.`when`(mockBluetoothGatt.device.bondState).thenReturn(0)
+
+
+         var actualDeviceList: DataState<DeviceDetails>? = null
+
+
+         val mockConnectionResult: MutableSharedFlow<DataState<DeviceDetails>> = mock()
+         bleConnectionManager.gattConnectionResult = mockConnectionResult
+
+
+         bleConnectionManager.onServicesDiscovered(mockBluetoothGatt, status)
+
+
+         val y:MutableSharedFlow<String> = mock()
+         bleConnectionManager.gc = y
+
+
+         val expectedMessage = "Hello"
+
+         val messages = y
+
+
+ //            y.take(1).collect{
+ //                println("sfsdfdsfdsf")
+ //                println(it)
+ //            }
+
+         val u = bleConnectionManager.gc.asSharedFlow().toList()
+
+         println("sfsdfdsfdsf")
+         println(u)
+
+
+         assertEquals(expectedMessage, messages.take(1))
+
+         //launch {
+         *//*bleConnectionManager.bleGattConnectionResult().take(1).toList().first()
+
+
+        val serviceCharacteristicsMap = extractServicesWithCharacteristics(mockBluetoothGatt.services)
+
+        val deviceInfo = DeviceInfo(
+            name = mockBluetoothGatt.device.name,
+            address = mockBluetoothGatt.device.address,
+            generalInfo = "${mockBluetoothGatt.device.bondState}"
+        )
+
+        val details = DeviceDetails(deviceInfo = deviceInfo, services = serviceCharacteristicsMap)
+
+        val expectedDeviceList = DataState.success(details)
+
+        assertEquals(expectedDeviceList, actualDeviceList)*//*
+
+        //coVerify { bleConnectionManager["emitAttributes"](mockBluetoothGatt) }
+    }*/
+
+    @Test
+    fun xyz(): Unit = runBlocking {
+        val status = BluetoothGatt.GATT_SUCCESS
+
+        val mockBluetoothGatt = mock(BluetoothGatt::class.java)
+
+        bleConnectionManager.onServicesDiscovered(mockBluetoothGatt, status)
+
+
+       // val y: Channel<String> = Channel(Channel.BUFFERED)
+        bleConnectionManager.gc = Channel(Channel.BUFFERED)
+
+
+        val expectedMessage = "Hello"
+
+        launch {
+            bleConnectionManager.gc.receiveAsFlow().collect {
+                println("sfsdfdsfdsf")
+                println(it)
+            }
+        }.join()
+
+
+
+    }
+
+    private fun extractServicesWithCharacteristics(serviceList: List<BluetoothGattService>): Map<Service, List<Characteristic>> =
+        serviceList.associate { service ->
+            val characteristics = service.characteristics.map { bleCharacteristic ->
+                Utility.extractCharacteristicInfo(bleCharacteristic)
+            }
+            val serviceReadableTitleName = Utility.getServiceName(service.uuid)
+            val newService = Service(serviceReadableTitleName, service.uuid.toString())
+            newService to characteristics
+        }
 
 }
