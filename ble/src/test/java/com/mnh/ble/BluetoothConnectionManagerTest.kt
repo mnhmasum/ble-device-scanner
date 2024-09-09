916660edf7dbbdbdcba97e3a909ee81f7136a91f
nazmul <mnhmasum@gmail.com>
2024-09-09 16:26:28 +0600

    Implement Unit Test for Bluetooth Device Discovery result (Attributes)

--- ble/src/test/java/com/mnh/ble/BluetoothConnectionManagerTest.kt
+++ ble/src/test/java/com/mnh/ble/BluetoothConnectionManagerTest.kt
@@ -3,20 +3,19 @@ package com.mnh.ble
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothGatt
-import android.bluetooth.BluetoothGattService
 import android.bluetooth.BluetoothProfile
 import android.content.Context
 import com.mnh.ble.bluetooth.bleconnection.BleConnectionManagerImpl
-import com.napco.utils.Utility
-import com.napco.utils.model.Characteristic
-import com.napco.utils.model.Service
+import com.napco.utils.DataState
+import com.napco.utils.model.DeviceDetails
+import junit.framework.Assert.assertEquals
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineDispatcher
 import org.junit.Before
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.anyString
@@ -33,16 +32,20 @@ class BluetoothConnectionManagerTest {
 
     private lateinit var bleConnectionManager: BleConnectionManagerImpl
 
-    private val dispatcher = TestCoroutineDispatcher()
-    //private val mockScope = TestCoroutineScope(dispatcher)
-
     private val mockScope = CoroutineScope(Dispatchers.Unconfined)
 
+    private val gattConnectionResult: MutableSharedFlow<DataState<DeviceDetails>> =
+        MutableSharedFlow(replay = 1)
 
     @Before
     fun setUp() {
         bleConnectionManager =
-            BleConnectionManagerImpl(mockContext, mockScope, mockBluetoothAdapter)
+            BleConnectionManagerImpl(
+                mockContext,
+                scope = mockScope,
+                mockBluetoothAdapter,
+                gattConnectionResult = gattConnectionResult
+            )
     }
 
     @Test
@@ -68,108 +71,36 @@ class BluetoothConnectionManagerTest {
         Mockito.verify(mockBluetoothGatt).discoverServices()
     }
 
-    /* @Test
-     fun `test onServicesDiscovered success`(): Unit = runBlockingTest{
-         val status = BluetoothGatt.GATT_SUCCESS
-
-         val mockBluetoothGatt = mock(BluetoothGatt::class.java)
-
-         val mockDevice = mock(BluetoothDevice::class.java)
-
-         Mockito.`when`(mockBluetoothGatt.device).thenReturn(mockDevice)
-
-         Mockito.`when`(mockBluetoothGatt.device.name).thenReturn("abc")
-         Mockito.`when`(mockBluetoothGatt.device.address).thenReturn("address")
-         Mockito.`when`(mockBluetoothGatt.device.bondState).thenReturn(0)
-
-
-         var actualDeviceList: DataState<DeviceDetails>? = null
-
-
-         val mockConnectionResult: MutableSharedFlow<DataState<DeviceDetails>> = mock()
-         bleConnectionManager.gattConnectionResult = mockConnectionResult
-
-
-         bleConnectionManager.onServicesDiscovered(mockBluetoothGatt, status)
-
-
-         val y:MutableSharedFlow<String> = mock()
-         bleConnectionManager.gc = y
-
-
-         val expectedMessage = "Hello"
-
-         val messages = y
-
-
- //            y.take(1).collect{
- //                println("sfsdfdsfdsf")
- //                println(it)
- //            }
-
-         val u = bleConnectionManager.gc.asSharedFlow().toList()
-
-         println("sfsdfdsfdsf")
-         println(u)
-
-
-         assertEquals(expectedMessage, messages.take(1))
-
-         //launch {
-         *//*bleConnectionManager.bleGattConnectionResult().take(1).toList().first()
-
-
-        val serviceCharacteristicsMap = extractServicesWithCharacteristics(mockBluetoothGatt.services)
-
-        val deviceInfo = DeviceInfo(
-            name = mockBluetoothGatt.device.name,
-            address = mockBluetoothGatt.device.address,
-            generalInfo = "${mockBluetoothGatt.device.bondState}"
-        )
-
-        val details = DeviceDetails(deviceInfo = deviceInfo, services = serviceCharacteristicsMap)
-
-        val expectedDeviceList = DataState.success(details)
-
-        assertEquals(expectedDeviceList, actualDeviceList)*//*
-
-        //coVerify { bleConnectionManager["emitAttributes"](mockBluetoothGatt) }
-    }*/
-
     @Test
-    fun xyz(): Unit = runBlocking {
+    fun `test onServicesDiscovered success`(): Unit = runBlocking {
         val status = BluetoothGatt.GATT_SUCCESS
 
         val mockBluetoothGatt = mock(BluetoothGatt::class.java)
 
+        val mockDevice = mock(BluetoothDevice::class.java)
+
+        Mockito.`when`(mockBluetoothGatt.device).thenReturn(mockDevice)
+
+        Mockito.`when`(mockBluetoothGatt.device.name).thenReturn("abc")
+        Mockito.`when`(mockBluetoothGatt.device.address).thenReturn("address")
+        Mockito.`when`(mockBluetoothGatt.device.bondState).thenReturn(0)
+
+        val job = launch {
+
+            val result = bleConnectionManager.bleGattConnectionResult().take(1).toList()
+
+            println(result.toString())
+
+            val expected = "[Success(data=DeviceDetails(deviceInfo=DeviceInfo(name=abc, address=address, generalInfo=0), services={}))]"
+
+            assertEquals(expected, result.toString())
+
+        }
+
         bleConnectionManager.onServicesDiscovered(mockBluetoothGatt, status)
 
-
-       // val y: Channel<String> = Channel(Channel.BUFFERED)
-        bleConnectionManager.gc = Channel(Channel.BUFFERED)
-
-
-        val expectedMessage = "Hello"
-
-        launch {
-            bleConnectionManager.gc.receiveAsFlow().collect {
-                println("sfsdfdsfdsf")
-                println(it)
-            }
-        }.join()
-
-
-
+        job.join()
     }
 
-    private fun extractServicesWithCharacteristics(serviceList: List<BluetoothGattService>): Map<Service, List<Characteristic>> =
-        serviceList.associate { service ->
-            val characteristics = service.characteristics.map { bleCharacteristic ->
-                Utility.extractCharacteristicInfo(bleCharacteristic)
-            }
-            val serviceReadableTitleName = Utility.getServiceName(service.uuid)
-            val newService = Service(serviceReadableTitleName, service.uuid.toString())
-            newService to characteristics
-        }
 
 }
