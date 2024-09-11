be732b6d9e9f8398805b89eddd7170bab28aa700
nazmul <mnhmasum@gmail.com>
2024-09-11 22:12:06 +0600

    Implement Unit Test for BLE characteristic write

--- ble/src/test/java/com/mnh/ble/BluetoothConnectionManagerTest.kt
+++ ble/src/test/java/com/mnh/ble/BluetoothConnectionManagerTest.kt
@@ -21,6 +21,7 @@ import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.anyString
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
+import java.util.UUID
 import kotlin.test.Test
 
 
@@ -91,13 +92,50 @@ class BluetoothConnectionManagerTest {
 
             println(result.toString())
 
-            val expected = "[Success(data=DeviceDetails(deviceInfo=DeviceInfo(name=abc, address=address, generalInfo=0), services={}))]"
+            val expected =
+                "[Success(data=DeviceDetails(deviceInfo=DeviceInfo(name=abc, address=address, generalInfo=0), services={}))]"
 
             assertEquals(expected, result.toString())
 
         }
 
         bleConnectionManager.onServicesDiscovered(mockBluetoothGatt, status)
+
+        job.join()
+    }
+
+    @Test
+    fun `test writeCharacteristic success`(): Unit = runBlocking {
+        val status = BluetoothGatt.GATT_SUCCESS
+
+        val mockBluetoothGatt = mock(BluetoothGatt::class.java)
+
+        val mockDevice = mock(BluetoothDevice::class.java)
+
+        Mockito.`when`(mockBluetoothGatt.device).thenReturn(mockDevice)
+
+        Mockito.`when`(mockBluetoothGatt.device.name).thenReturn("abc")
+        Mockito.`when`(mockBluetoothGatt.device.address).thenReturn("address")
+        Mockito.`when`(mockBluetoothGatt.device.bondState).thenReturn(0)
+
+        val job = launch {
+
+            val result = bleConnectionManager.bleGattConnectionResult().take(1).toList()
+
+            println(result.toString())
+
+            val expected =
+                "[Success(data=DeviceDetails(deviceInfo=DeviceInfo(name=abc, address=address, generalInfo=0), services={}))]"
+
+            assertEquals(expected, result.toString())
+
+        }
+
+        val serviceID = UUID.randomUUID()
+        val characteristicsID = UUID.randomUUID()
+        val byte = ByteArray(3)
+
+        bleConnectionManager.writeCharacteristic(serviceID, characteristicsID, byte)
 
         job.join()
     }
